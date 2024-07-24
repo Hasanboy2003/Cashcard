a55201de8d2816cea629647d48580be3714d5627
maxmudovhasanboy070 <maxmudovhasanboy070@gmail.com>
2024-07-24 20:03:01 +0500

    #Lab: Implementing GET with pagination and sort

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,48 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
 plugins {
-	java
-	id("org.springframework.boot") version "3.3.2"
-	id("io.spring.dependency-management") version "1.1.6"
+    java
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.data:spring-data-jdbc")
+    implementation("com.h2database:h2")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+tasks {
+    test {
+        testLogging {
+            events("passed", "skipped", "failed")
+
+            showExceptions = true
+            exceptionFormat = TestExceptionFormat.FULL
+            showCauses = true
+            showStackTraces = true
+
+            // Change to `true` for more verbose test output
+            showStandardStreams = true
+        }
+    }
 }
